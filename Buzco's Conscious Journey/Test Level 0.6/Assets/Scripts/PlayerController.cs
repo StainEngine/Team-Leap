729ed4e372a5413cdf03de4686844dc73617a4b7
Ecte <djfran86@gmail.com>
2020-02-03 11:58:33 +1030

    Added camera control
    Now v0.6

---
+++ Buzco's Conscious Journey/Test Level 0.6/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    //public Rigidbody theRB;
+    public float jumpForce;
+    public CharacterController controller;
+    private Vector3 moveDirection;
+    public float gravityScale;
+
+    private int numJumps = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //theRB = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //theRB.velocity = new Vector3(Input.GetAxis("Horizontal")*moveSpeed,theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+        /*if (Input.GetButtonDown("Jump"))
+        {
+            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+        }*/
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+        if (controller.isGrounded)// || numJumps<2)
+            {
+            if (Input.GetButtonDown("Jump"))
+            {
+                    moveDirection.y = jumpForce;
+            }
+        }
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        controller.Move(moveDirection*Time.deltaTime);
+    }
+}
